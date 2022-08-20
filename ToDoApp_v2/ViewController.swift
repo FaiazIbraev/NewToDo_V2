8c95b8124b25a387831d98afbcd117abffa26f8f
FaiazIbraev <fayaz.ibraev@gmail.com>
2022-08-20 17:56:54 +0600

    Initial Commit

---
+++ ToDoApp_v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoApp_v2
+//
+//  Created by Faiaz Ibraev on 20/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
